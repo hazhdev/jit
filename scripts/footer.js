35e8c9b03d0f668f1be153f80c937853f63b6a01
hazhdev <hazhbekar.zamaev@gmail.com>
2025-01-03 11:57:38 +0300

    appsent

--- scripts/footer.js
+++ scripts/footer.js
@@ -4,10 +4,34 @@ const footermagazin = document.querySelector('.footer__magazin');
 const footerinfaList = footerinfa.nextElementSibling;
 const footermagazinList = footermagazin.nextElementSibling;
 
+if (window.innerWidth > 600) {
+    list.style.display = 'block';
+} 
+
 footerinfa.addEventListener('click', () => {
-    footerinfaList.style.display = footerinfaList.style.display === 'none' || footerinfaList.style.display === '' ? 'block' : 'none';
+    if (footerinfaList.style.display === 'none' || footerinfaList.style.display === '') {
+        footerinfaList.style.display = 'block';
+    } else {
+        footerinfaList.style.display = 'none';
+    }
 });
 
 footermagazin.addEventListener('click', () => {
-    footermagazinList.style.display = footermagazinList.style.display === 'none' || footermagazinList.style.display === '' ? 'block' : 'none';
+    if (footermagazinList.style.display === 'none' || footermagazinList.style.display === '') {
+        footermagazinList.style.display = 'block';
+    } else {
+        footermagazinList.style.display = 'none';
+    }
 });
+
+
+function setListDisplay(list) {
+    if (window.innerWidth > 600) {
+        list.style.display = 'block'; // В десктопной версии всегда показываем список
+    }
+}
+;
+
+// Устанавливаем начальное состояние для десктопа
+setListDisplay(footerinfaList);
+setListDisplay(footermagazinList);
