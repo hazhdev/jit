7ed9ed5ea895957e8597fa5e0446445a4fd4b2a4
hazhdev <hazhbekar.zamaev@gmail.com>
2025-01-08 00:47:31 +0300

    main

--- scripts/footer.js
+++ scripts/footer.js
@@ -4,10 +4,6 @@ const footermagazin = document.querySelector('.footer__magazin');
 const footerinfaList = footerinfa.nextElementSibling;
 const footermagazinList = footermagazin.nextElementSibling;
 
-if (window.innerWidth > 600) {
-    list.style.display = 'block';
-} 
-
 footerinfa.addEventListener('click', () => {
     if (footerinfaList.style.display === 'none' || footerinfaList.style.display === '') {
         footerinfaList.style.display = 'block';
@@ -25,13 +21,6 @@ footermagazin.addEventListener('click', () => {
 });
 
 
-function setListDisplay(list) {
-    if (window.innerWidth > 600) {
-        list.style.display = 'block'; // В десктопной версии всегда показываем список
-    }
-}
-;
-
-// Устанавливаем начальное состояние для десктопа
-setListDisplay(footerinfaList);
-setListDisplay(footermagazinList);
+if (window.innerWidth > 600) {
+    list.style.display = 'block';
+} 
